bcf9c9cfa19a0420a610d28e8125804768791a90
Vidwath K T <vidwathveerashiva63@gmail.com>
2024-12-27 13:03:27 +0530

    [Vidwath]Added:Implemented logger using winston and morgan

---
+++ src/config/logger.ts
@@ -0,0 +1,65 @@
+import winston, { format } from 'winston';
+import 'winston-daily-rotate-file';
+
+
+const createLogger = () => {
+  return winston.createLogger({
+    format: format.combine(format.timestamp(), format.simple()),
+    transports: [
+      new winston.transports.File({
+        filename: 'logfiles/logs.log',
+        level: 'error',
+        handleExceptions: true,
+      }),
+      new winston.transports.File({
+        filename: 'logfiles/logs.log',
+        level: 'info',
+        handleExceptions: true,
+      }),
+      new winston.transports.DailyRotateFile({
+        maxFiles: '14d',
+        level: 'info',
+        dirname: 'logfiles',
+        datePattern: 'YYYY-MM-DD',
+        filename: 'logs.log',
+      }),
+      new winston.transports.Console({
+        level: 'debug',
+        handleExceptions: true,
+      }),
+    ],
+  });
+};
+
+
+const createMorganLogger = () => {
+  return winston.createLogger({
+    format: format.combine(format.simple()),
+    transports: [
+      new winston.transports.File({
+        filename: 'logfiles/logs.log',
+        level: 'debug',
+        handleExceptions: true,
+      }),
+      new winston.transports.Console({
+        level: 'debug',
+        handleExceptions: true,
+      }),
+      new winston.transports.DailyRotateFile({
+        maxFiles: '14d',
+        level: 'info',
+        dirname: 'logfiles',
+        datePattern: 'YYYY-MM-DD',
+        filename: 'logs.log',
+      }),
+    ],
+  });
+};
+
+
+const logStream = (message: any): void => {
+  const morganLogger = createMorganLogger();
+  morganLogger.info(message.toString());
+};
+
+export { createLogger, logStream };
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express'
 import { db} from './config/DB.config'
 import routes from './routes/index.route'
+import {createLogger} from './config/logger';
+import morgan from 'morgan';
 
 dotenv.config();
 
@@ -10,8 +12,16 @@ const host = process.env.APP_HOST;
 const port = process.env.APP_PORT;
 const version = process.env.API_VERSION;
 
+const logger = createLogger();
+const morgonLogStream = {
+    write: (message: string): void => {
+      logger.info(message.trim()); 
+    },
+  };
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(morgan('combined', { stream: morgonLogStream }));
 
 app.use(`/api/${version}`,routes());
 
