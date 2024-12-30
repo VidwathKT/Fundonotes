064865a655c73ba9896544e2c92ba7c9d9682b5b
Vidwath K T <vidwathveerashiva63@gmail.com>
2024-12-30 14:45:47 +0530

    [Vidwath]Added:interface and model of notes

---
+++ src/models/note.model.ts
@@ -0,0 +1,20 @@
+import { Schema, model,Types} from 'mongoose';
+import { Inote } from '../interfaces/note.interface';
+
+
+const noteSchema = new Schema<Inote>(
+  {
+    title:{type: String},
+    description: {type: String},
+    color: {type: String,default: '#FFFFFF' },
+    isArchive: {type: Boolean,default:false},
+    isTrash: {type: Boolean,default:false},
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User',optional:true },
+  },
+  {
+    timestamps: true,        
+  }
+);
+
+
+export default model('Note', noteSchema);
